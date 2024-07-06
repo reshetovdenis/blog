ce41ea0fc1fa0f368702d2ff924aa27154c138c3
Reshetov Denis <reshetovdenis@gmail.com>
2024-07-06 10:22:26 +0200

    Capture all elements

--- screenshot.py
+++ screenshot.py
@@ -7,7 +7,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from PIL import Image
 import io
-import time
 
 def capture_screenshots(url, class_name):
     # Configure Selenium to use Chrome
@@ -20,22 +19,29 @@ def capture_screenshots(url, class_name):
         # Open the webpage
         driver.get(url)
         
-        # Wait until the element is present
-        element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, class_name))
+        # Wait until the elements are present
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.CLASS_NAME, class_name))
         )
-        #time.sleep(2)  # Ensure the page is fully loaded
- 
-        image_binary = element.screenshot_as_png 
-        img = Image.open(io.BytesIO(image_binary))
-        img.save("image.png")
+
+        # Find all elements with the given class name
+        elements = driver.find_elements(By.CLASS_NAME, class_name)
+
+        if elements:
+            for index, element in enumerate(elements):
+                # Capture screenshot of each element
+                image_binary = element.screenshot_as_png 
+                img = Image.open(io.BytesIO(image_binary))
+                img.save(f"element_{index + 1}.png")
+        else:
+            print("No elements found with the specified class name.")
 
     finally:
         driver.quit()
 
 # Example usage
-# url = 'https://app.slonig.org/#/knowledge?id=0xd86f01db4b3157dd34268122e6ba45895632e2406b8011c54982024b2180a550'
-url = 'https://app.slonig.org/#/knowledge?id=0x10bddf453ccd8118d85521ac958e6fd8ff133d688f326d4ff36e301a638c28fe'
+url = 'https://app.slonig.org/#/knowledge?id=0xd86f01db4b3157dd34268122e6ba45895632e2406b8011c54982024b2180a550'
+#url = 'https://app.slonig.org/#/knowledge?id=0x10bddf453ccd8118d85521ac958e6fd8ff133d688f326d4ff36e301a638c28fe'
 
 class_name = 'exercise-display'
 
