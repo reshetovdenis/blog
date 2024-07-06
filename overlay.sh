48bce7c523147839afb8e779bb8908bae3412117
Reshetov Denis <reshetovdenis@gmail.com>
2024-07-06 13:28:12 +0200

    center

--- overlay.sh
+++ overlay.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Check if the correct number of arguments are provided
-if [ "$#" -ne 4 ]; then
-  echo "Usage: $0 input_video overlay_image output_video position"
-  echo "Position options: topleft, topright, bottomleft, bottomright"
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 input_video overlay_image output_video"
   exit 1
 fi
 
@@ -11,27 +10,9 @@ fi
 INPUT_VIDEO="$1"
 OVERLAY_IMAGE="$2"
 OUTPUT_VIDEO="$3"
-POSITION="$4"
 
-# Determine overlay position
-case "$POSITION" in
-  topleft)
-    OVERLAY_POSITION="10:10"
-    ;;
-  topright)
-    OVERLAY_POSITION="main_w-overlay_w-10:10"
-    ;;
-  bottomleft)
-    OVERLAY_POSITION="10:main_h-overlay_h-10"
-    ;;
-  bottomright)
-    OVERLAY_POSITION="main_w-overlay_w-10:main_h-overlay_h-10"
-    ;;
-  *)
-    echo "Invalid position option. Use: topleft, topright, bottomleft, bottomright."
-    exit 1
-    ;;
-esac
+# Calculate the overlay position
+OVERLAY_POSITION="(main_w-overlay_w)/2:(main_h*0.25)"
 
 # Execute the ffmpeg command to overlay the image
 ffmpeg -i "$INPUT_VIDEO" -i "$OVERLAY_IMAGE" -filter_complex "overlay=$OVERLAY_POSITION" -codec:a copy "$OUTPUT_VIDEO"
